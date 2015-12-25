47da5a5dd032c95295d8252e2df1f9d86501a237
bg1avd <raolinmobile@gmail.com>
2015-12-25 03:26:59 -0800

    Add normal time format

--- public/moduleTime.js
+++ public/moduleTime.js
@@ -5,7 +5,10 @@ module.exports = {
     var hour = addZero(time.getHours());
     var minutes = addZero(time.getMinutes());
     var seconds = addZero(time.getSeconds());
-    return "#" + hour + minutes + seconds
+    var colorval = "#" + hour + minutes + seconds;
+    var timeval = hour +':'+ minutes + ':'+seconds;
+	  var json = JSON.stringify({"color":colorval,"time":timeval});
+	  return json
   }
 
 };
